4abdc9c08f8e8b006722672ccff34c7681652a28
andrewliebchen <andrewliebchen@gmail.com>
2014-09-27 15:33:34 -0700

    Down the road to simplicity

--- Stadiuuum.js
+++ Stadiuuum.js
@@ -1,15 +1,44 @@
-Ideas = new Meteor.Collection('ideas');
-Tasks = new Meteor.Collection('tasks');
-Todos = new Meteor.Collection('todos');
+Themes = new Meteor.Collection('themes');
+Tasks  = new Meteor.Collection('tasks');
 
 if (Meteor.isClient) {
-  Template.ideas.idea = function() {
-    return Ideas.find({});
+  Session.set('showChat', null);
+  Session.set('activeItem', null);
+  Session.set('showTasks', null);
+
+  Template.themes.theme = function() {
+    return Themes.find({});
   };
+
+  Template.themes.isActive = function() {
+    return Session.equals('activeItem', this._id);
+  }
+
+  Template.themes.showTasks = function() {
+    return Session.equals('showTasks', this._id);
+  }
 
   Template.tasks.task = function() {
     return Tasks.find({}, {sort: {number: 1}});
   }
+
+  Template.chat.showChat = function() {
+    return !Session.equals('showChat', null);
+  }
+
+  Template.themes.events({
+    'click .mtr_toggle-chat': function(event) {
+      event.preventDefault();
+      Session.set('showChat', this._id);
+      Session.set('activeItem', this._id);
+    },
+
+    'click .mtr_show-tasks': function(event) {
+      event.preventDefault();
+      Session.set('showTasks', this._id);
+      Session.set('activeItem', this._id);
+    }
+  });
 }
 
 if (Meteor.isServer) {
--- seed.js
+++ seed.js
@@ -1,69 +1,90 @@
 if (Meteor.isServer) {
-  if(Ideas.find().count() === 0) {
-    Ideas.insert({
-      title:       'A project management system that minimally tracks the entire product life cycle, from idea to development to documentation',
-      description: 'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
-      tag:         'big idea',
-      number:      1,
-      priority:    0
+  Themes.remove({});
+  if(Themes.find().count() === 0) {
+    Themes.insert({
+      title:         'Themes and teams',
+      description:   'The app should focus on big product ideas and the groups of people that get those ideas done',
+      number:        1,
+      priorityCount: 0
+    });
+
+    Themes.insert({
+      title:         'Mauris iaculis porttitor',
+      description:   'Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus',
+      number:        2,
+      priorityCount: 0
+    });
+
+    Themes.insert({
+      title:         'Curabitur vulputate, ligula lacinia scelerisque',
+      description:   'Sed molestie augue sit amet leo consequat posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere',
+      number:        3,
+      priorityCount: 0
+    });
+
+    Themes.insert({
+      title:         'Nunc eu ullamcorper orci, quisque',
+      description:   'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices',
+      number:        4,
+      priorityCount: 0
     });
   }
 
   if(Tasks.find().count() === 0) {
     Tasks.insert({
-      title:       'Add ideas so that I can keep track of them',
-      description: 'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
-      number:      2,
-      priority:    0,
-      loe:         0
+      title:         'Add Themes so that I can keep track of them',
+      description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
+      number:        2,
+      priorityCount: 0,
+      loe:           0
     });
 
     Tasks.insert({
-      title:       'View a list of all my ideas',
-      description: 'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
-      number:      3,
-      priority:    0,
-      loe:         0
+      title:         'View a list of all my ideas',
+      description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
+      number:        3,
+      priorityCount: 0,
+      loe:           0
     });
 
     Tasks.insert({
-      title:       'Tag ideas so that I can organize them',
-      description: 'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
-      number:      4,
-      priority:    0,
-      loe:         0
+      title:         'Tag ideas so that I can organize them',
+      description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
+      number:        4,
+      priorityCount: 0,
+      loe:           0
     });
 
     Tasks.insert({
-      title:       'Add tasks to an idea so I know what needs needs to be done to complete an idea',
-      description: 'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
-      number:      5,
-      priority:    0,
-      loe:         0
+      title:         'Add tasks to an idea so I know what needs needs to be done to complete an idea',
+      description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
+      number:        5,
+      priorityCount: 0,
+      loe:           0
     });
 
     Tasks.insert({
-      title:       'Start and stop working on task',
-      description: 'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
-      number:      6,
-      priority:    0,
-      loe:         0
+      title:         'Start and stop working on task',
+      description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
+      number:        6,
+      priorityCount: 0,
+      loe:           0
     });
 
     Tasks.insert({
-      title:       'Vote on the priority of tasks',
-      description: 'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
-      number:      7,
-      priority:    0,
-      loe:         0
+      title:         'Vote on the priorityCount of tasks',
+      description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
+      number:        7,
+      priorityCount: 0,
+      loe:           0
     });
 
     Tasks.insert({
-      title:       'Vote on the level of efforts of tasks',
-      description: 'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
-      number:      8,
-      priority:    0,
-      loe:         0
+      title:         'Vote on the level of efforts of tasks',
+      description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
+      number:        8,
+      priorityCount: 0,
+      loe:           0
     });
   }
 }
