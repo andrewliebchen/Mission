86e009532becd4e9408571acf6e5d9994f9204a2
andrewliebchen <andrewliebchen@gmail.com>
2014-09-28 11:55:05 -0700

    Add route to single theme

--- Stadiuuum.js
+++ Stadiuuum.js
@@ -4,7 +4,6 @@ Tasks  = new Meteor.Collection('tasks');
 if (Meteor.isClient) {
   Session.set('showChat', null);
   Session.set('activeItem', null);
-  Session.set('showTasks', null);
 
   Template.themes.theme = function() {
     return Themes.find({});
@@ -12,19 +11,15 @@ if (Meteor.isClient) {
 
   Template.themes.isActive = function() {
     return Session.equals('activeItem', this._id);
-  }
-
-  Template.themes.showTasks = function() {
-    return Session.equals('showTasks', this._id);
-  }
+  };
 
   Template.tasks.task = function() {
     return Tasks.find({}, {sort: {number: 1}});
-  }
+  };
 
   Template.chat.showChat = function() {
     return !Session.equals('showChat', null);
-  }
+  };
 
   Template.themes.events({
     'click .mtr_toggle-chat': function(event) {
@@ -33,14 +28,23 @@ if (Meteor.isClient) {
       Session.set('activeItem', this._id);
     },
 
-    'click .mtr_show-tasks': function(event) {
+    'click .mtr_show-theme': function(event) {
       event.preventDefault();
-      Session.set('showTasks', this._id);
-      Session.set('activeItem', this._id);
+      Router.go('theme', {_id: this._id});
     }
   });
 }
 
 if (Meteor.isServer) {
+  Meteor.publish('themes', function() {
+    return Themes.find({});
+  });
 
+  Meteor.publish('theme', function(id) {
+    return id && Themes.find(id);
+  });
+
+  Meteor.publish('tasks', function() {
+    return Tasks.find({});
+  });
 }
---
+++ routes.js
@@ -0,0 +1,33 @@
+Router.configure({
+  layoutTemplate: 'application',
+  loadingTemplate: 'loading'
+});
+
+Router.map(function() {
+  this.route('home', {
+    path: '/',
+    waitOn: function() {
+      return Meteor.subscribe('themes');
+    },
+    data: function() {
+      return Themes.find({});
+    }
+  });
+
+  this.route('theme', {
+    path: '/theme/:_id',
+    waitOn: function() {
+      return [
+        Meteor.subscribe('theme', this.params._id),
+        Meteor.subscribe('tasks', this.params._id)
+      ];
+    },
+    data: function() {
+      // Session.set('currentTheme', this.params._id);
+      return Themes.findOne(this.params._id);
+    },
+    onStop: function() {
+      // Session.set('currentTheme', null);
+    }
+  });
+});
