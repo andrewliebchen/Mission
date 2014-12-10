56e9f739e021dec17e4e6d2b488d313d57694d86
andrewliebchen <andrewliebchen@gmail.com>
2014-12-09 23:38:18 -0800

    Status and remove Bourbon

--- seed.js
+++ seed.js
@@ -35,56 +35,49 @@ if (Meteor.isServer) {
       title:         'Add Themes so that I can keep track of them',
       description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
       number:        2,
-      priorityCount: 0,
-      loe:           0
+      status:        'todo'
     });
 
     Tasks.insert({
       title:         'View a list of all my ideas',
       description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
       number:        3,
-      priorityCount: 0,
-      loe:           0
+      status:        'todo'
     });
 
     Tasks.insert({
       title:         'Tag ideas so that I can organize them',
       description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
       number:        4,
-      priorityCount: 0,
-      loe:           0
+      status:        'todo'
     });
 
     Tasks.insert({
       title:         'Add tasks to an idea so I know what needs needs to be done to complete an idea',
       description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
       number:        5,
-      priorityCount: 0,
-      loe:           0
+      status:        'todo'
     });
 
     Tasks.insert({
       title:         'Start and stop working on task',
       description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
       number:        6,
-      priorityCount: 0,
-      loe:           0
+      status:        'todo'
     });
 
     Tasks.insert({
       title:         'Vote on the priorityCount of tasks',
       description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
       number:        7,
-      priorityCount: 0,
-      loe:           0
+      status:        'todo'
     });
 
     Tasks.insert({
       title:         'Vote on the level of efforts of tasks',
       description:   'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.',
       number:        8,
-      priorityCount: 0,
-      loe:           0
+      status:        'todo'
     });
   }
 }
